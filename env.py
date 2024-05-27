4142a78e5ed4470b2672ba3cac695a5c13411c00
Werner <wstablein@hotmail.com>
2024-05-27 13:54:54 +0200

    Add database connection

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://blog_owner:5x3RazOpEmYM@ep-autumn-mountain-a2k1mbf9.eu-central-1.aws.neon.tech/blog?sslmode=require")
+    "DATABASE_URL", "postgres://ux8cdzuvabm:Fz8MrfiraErR@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/satin_rage_floss_718428")
 
 os.environ.setdefault("SECRET_KEY", "django-insecure-@i0si+_fgixdwxmsn0yds!nwsk19&01fq2c%lr5kbgbkvulm$b")
