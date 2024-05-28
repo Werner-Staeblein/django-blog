bfbe964a72c4f1829b8889bad620b53156796ca2
Werner <wstablein@hotmail.com>
2024-05-28 11:34:48 +0200

    Add Meta and dunder to comment Class

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     # excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"The title of this post is {self.title}"
+
 
 
 class Comment(models.Model):
@@ -22,3 +28,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
