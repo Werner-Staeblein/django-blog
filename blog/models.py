c3e15ef0c2c650968bb859136a385d72e5b473bd
Werner <wstablein@hotmail.com>
2024-05-28 15:22:24 +0200

    Adjust models.py back to original after simulating changes to attributes

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
-
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -14,12 +13,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     # excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
     class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
         return f"The title of this post is {self.title}"
-
 
 
 class Comment(models.Model):
