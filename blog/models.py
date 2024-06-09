fa7ba72995fe790835655d740d558d77e2090bcb
Werner <wstablein@hotmail.com>
2024-06-09 11:30:40 +0200

    Write urls.py for django project and about app. Test functionality

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# excerpt = models.TextField(blank=True)
+
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -11,7 +13,6 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    # excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     
     class Meta:
