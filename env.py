6ea847678179d991bc9224e9fa24b05d219fe896
Werner <wstablein@hotmail.com>
2024-06-20 11:03:42 +0200

    Enable servings of image files with cloudinary

--- env.py
+++ env.py
@@ -4,3 +4,7 @@ os.environ.setdefault(
     "DATABASE_URL", "postgres://ux8cdzuvabm:Fz8MrfiraErR@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/satin_rage_floss_718428")
 
 os.environ.setdefault("SECRET_KEY", "iuutfrjjBFJQx8Mc3GWIiiQQmcUDraNI")
+
+os.environ.setdefault("CLOUDINARY_URL", "cloudinary://https://console.cloudinary.com/pm/c-50284f13815aa2846a3504698117e9/developer-dashboard")
+
+
